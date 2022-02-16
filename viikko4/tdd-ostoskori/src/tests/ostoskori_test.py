8c8de5c1a089d10a78ccd5bfdba32bc2b549b0d2
Peruna <noora.matilainen@helsinki.fi>
2022-02-16 10:06:53 +0200

    vk4 t5.5

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,7 +19,7 @@ class TestOstoskori(unittest.TestCase):
     def test_yhden_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteen_hinta(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
-        
+
         self.assertEqual(self.kori.hinta(), maito.hinta())
 
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_2_tavaraa(self):
@@ -29,4 +29,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(mehu)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        mehu = Tuote("Mehu", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(mehu)
+
+        self.assertEqual(self.kori.hinta(), maito.hinta() + mehu.hinta())
         
