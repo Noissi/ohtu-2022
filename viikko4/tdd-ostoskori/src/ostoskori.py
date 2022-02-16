f6644ebd1cdd2840274d7d5afb362e87e072616a
Peruna <noora.matilainen@helsinki.fi>
2022-02-16 09:57:42 +0200

    vk4 t5.2

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -4,20 +4,25 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self._ostokset = []
+        self._ostokset = {}
 
     def tavaroita_korissa(self):
-        maara = map(lambda t: t.lukumaara, self._ostokset)
+        ostokset = list(self._ostokset.values())
+        maara = map(lambda t: t.lukumaara(), ostokset)
         return sum(maara)
         
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        hinta = map(lambda t: t.hinta, self._ostokset)
+        ostokset = list(self._ostokset.values())
+        hinta = map(lambda t: t.hinta, ostokset)
         return sum(hinta)
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        pass
+        if lisattava in self._ostokset:
+            self._ostokset[lisattava].muuta_lukumaaraa(1)
+        else:
+            self._ostokset[lisattava] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
